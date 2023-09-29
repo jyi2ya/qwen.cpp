baf93deda8d528069b5654203a781f3a64404800
jyi2ya <jyi2ya@outlook.com>
2023-09-29 17:18:18 +0800

    feat: 加入 Pipeline 绑定和 `chat` 和 `stream_chat` 函数。
    妈的，Python 和 C++ 没一个会的，人麻了

--- qwen_pybind.cpp
+++ qwen_pybind.cpp
@@ -1,4 +1,5 @@
 #include "tiktoken.h"
+#include "qwen.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
@@ -16,6 +17,27 @@ PYBIND11_MODULE(_C, m) {
     .def("encode", &tiktoken::tiktoken::encode)
     .def("encode_single_piece", &tiktoken::tiktoken::encode_single_piece)
     .def("decode", &tiktoken::tiktoken::decode);
+
+    py::class_<GenerationConfig>(m, "GenerationConfig")
+        .def(py::init<int, int, bool, int, float, float, float, int>(), "max_length"_a = 2048,
+             "max_context_length"_a = 512, "do_sample"_a = true, "top_k"_a = 0, "top_p"_a = 0.7, "temperature"_a = 0.95,
+             "repetition_penalty"_a = 1.0, "num_threads"_a = 0)
+        .def_readwrite("max_length", &GenerationConfig::max_length)
+        .def_readwrite("max_context_length", &GenerationConfig::max_context_length)
+        .def_readwrite("do_sample", &GenerationConfig::do_sample)
+        .def_readwrite("top_k", &GenerationConfig::top_k)
+        .def_readwrite("top_p", &GenerationConfig::top_p)
+        .def_readwrite("temperature", &GenerationConfig::temperature)
+        .def_readwrite("repetition_penalty", &GenerationConfig::repetition_penalty)
+        .def_readwrite("num_threads", &GenerationConfig::num_threads);
+
+
+    // ===== Pipeline ====
+
+  py::class_<Pipeline>(m, "Pipeline")
+      .def(py::init<const std::string &, const std::string &>())
+      .def_property_readonly("model", [](const Pipeline &self) { return self.model.get(); })
+      .def_property_readonly("tokenizer", [](const Pipeline &self) { return self.tokenizer.get(); });
 }
 
 } // namespace qwen
